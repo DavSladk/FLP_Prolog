69845bde881370fdff892b16808252d9aa063291
DavSladk <davsladk+github@gmail.com>
2022-04-27 11:38:37 +0200

    Added test script.

--- makefile
+++ makefile
@@ -1,13 +1,16 @@
-.PHONY: compile run zip clean
+.PHONY: compile run zip clean test
 
 compile: src.pl
-	swipl -q -g main -o flp21-log -c src.pl
+	swipl -g main -o flp21-log -c src.pl
 
 run:
 	./flp21-log < tests/test1.in
 
 clean:
-	rm -f flp21-log flp-log-xsladk07.zip
+	rm -f flp21-log flp-log-xsladk07.zip tests/tests.out
 
 zip: clean
 	zip flp-log-xsladk07.zip src.pl makefile README.md tests/*
+
+test: compile
+	bash test.sh
--- test.sh
+++ test.sh
@@ -1,1 +1,41 @@
 #!/bin/bash
+# Project: Kostra grafu (spanning tree)
+# Author: Bc. David Sladký
+# Login: xsladk07
+
+rm -f tests/tests.out
+REPEATS=5
+
+echo '## Tests' > README.md
+echo 'This README is regenerated for every run of test.sh.' >> README.md
+echo 'Original times mesured on merlin.' >> README.md
+echo >> README.md
+
+echo -n '| Test |' >> README.md
+for ((i=1;i<=REPEATS;i++)); do
+    echo -n " time $i |" >> README.md
+done
+echo " AVG |" >> README.md
+
+echo -n '| --- |' >> README.md
+for ((i=1;i<=REPEATS;i++)); do
+    echo -n " --- |" >> README.md
+done
+echo ' --- |' >> README.md
+
+
+for test in tests/*.in; do
+    totalTime=0
+    echo -n "| $test |" >> README.md
+    for ((i=1;i<=REPEATS;i++)); do
+        startTime=$(date +%s%N)
+        ./flp21-log < $test >> tests/tests.out
+        endTime=$(date +%s%N)
+        diffTime=$((($endTime-$startTime)/1000000))
+        totalTime=$(($totalTime+$diffTime))
+        echo -n " $diffTime ms |" >> README.md
+
+    done
+    avgTime=$(($totalTime/$REPEATS))
+    echo " $avgTime ms |" >> README.md
+done

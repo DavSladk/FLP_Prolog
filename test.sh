ee71fe7534e16ab77594505d71f7aeb2b5106625
DavSladk <davsladk+github@gmail.com>
2022-04-27 11:50:42 +0200

    Added comments

--- test.sh
+++ test.sh
@@ -6,24 +6,27 @@
 rm -f tests/tests.out
 REPEATS=5
 
+# Prints header line
 echo '## Tests' > README.md
 echo 'This README is regenerated for every run of test.sh.' >> README.md
 echo 'Original times mesured on merlin.' >> README.md
 echo >> README.md
 
+# Print table header
 echo -n '| Test |' >> README.md
 for ((i=1;i<=REPEATS;i++)); do
     echo -n " time $i |" >> README.md
 done
 echo " AVG |" >> README.md
 
+# Prints table header separator
 echo -n '| --- |' >> README.md
 for ((i=1;i<=REPEATS;i++)); do
     echo -n " --- |" >> README.md
 done
 echo ' --- |' >> README.md
 
-
+# Executes tests, measure their duration and prints the results
 for test in tests/*.in; do
     totalTime=0
     echo -n "| $test |" >> README.md
